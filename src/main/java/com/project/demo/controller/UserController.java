47b19d06bb2073b7e2124995bcfbcf5c79c0d001
kbs99 <126662389+KBSudHanVa@users.noreply.github.com>
2026-04-04 03:22:49 +0530

    first commit

---
+++ src/main/java/com/project/demo/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.project.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.demo.model.Users;
+import com.project.demo.repo.UserRepo;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+	
+	@Autowired
+	UserRepo repo;
+
+	@GetMapping("/name")
+	public String name() {
+		return "sudhanva";
+	}
+	
+	@GetMapping("/users")
+	public List<Users> users() {
+		return repo.findAll();
+	}
+	
+}
---
+++ src/main/java/com/project/demo/repo/UserRepo.java
@@ -0,0 +1,11 @@
+package com.project.demo.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project.demo.model.Users;
+
+@Repository
+public interface UserRepo extends JpaRepository<Users, String>  {
+
+}
