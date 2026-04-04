6ea2ced57d8c11a95e59e11b52643a83bfc07c32
kbs99 <126662389+KBSudHanVa@users.noreply.github.com>
2026-04-05 02:28:54 +0530

    v6

--- src/main/java/com/project/demo/controller/UserController.java
+++ src/main/java/com/project/demo/controller/UserController.java
@@ -1,26 +1,20 @@
 package com.project.demo.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.project.demo.model.Users;
-import com.project.demo.repo.UserRepo;
-
 @RestController
 @RequestMapping("/api")
 public class UserController {
 	
-	@Autowired
-	UserRepo repo;
+//	@Autowired
+//	UserRepo repo;
 
 	@GetMapping("/name")
 	public String name() {
-		return "sudhanva-v5";
+		return "sudhanva-v6";
 	}
 	
 //	@GetMapping("/users")
